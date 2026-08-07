1e8a71e2369964614312778e66f105e079252027
soruto752 <sekoniireoluwa@gmail.com>
2026-08-07 18:29:15 +0100

    67676767676766767676767676

---
+++ js/mermory.js
@@ -0,0 +1,22 @@
+const cars = ["saab", "volvo", "bmw"];
+let car = cars[1];
+cars[1] = "toyato";
+console.log(cars);
+//1
+const stundent = ["ireoluwa", "9", "css", "false"];
+console.log("stundent");
+//2
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[1] = "pineapple";
+console.log(fruits);
+let fruitstring = fruits.toString;
+console.log("fruitstring");
+//3
+const myData = [
+  "Hello World",
+  42,
+  true,
+  { name: "Alex", age: 30 },
+  ["apple", "banana", "cherry"],
+];
+console.log(myData);
