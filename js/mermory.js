1c4a0d60a6331c9c7a6730b93ef7938ca2f64bdc
soruto752 <sekoniireoluwa@gmail.com>
2026-08-07 18:50:14 +0100

    4w3q4rfr4ffvr4fr4v4zrfvyui4zrfvmk

--- js/mermory.js
+++ js/mermory.js
@@ -20,3 +20,29 @@ const myData = [
   ["apple", "banana", "cherry"],
 ];
 console.log(myData);
+//4
+let cars = [];
+
+cars[0] = "Toyota";
+cars[1] = "Honda";
+cars[2] = "Tesla";
+cars[3] = "BMW";
+
+cars[2] = "Mercedes";
+
+console.log("First element:", cars[0]);
+console.log("Last element:", cars[cars.length - 1]);
+console.log("Complete array:", cars);
+
+let movies = ["Inception", "The Matrix", "Interstellar", "Gladiator", "Avatar"];
+
+console.log("First movie:", movies[0]);
+console.log("Third movie:", movies[2]);
+console.log("Last movie:", movies[movies.length - 1]);
+
+movies[1] = "The Dark Knight";
+
+console.log("Total number of movies:", movies.length);
+
+let moviesString = movies.tostring;
+console.log(" moviesString");
