4a01bcb7f23aeb4a61cb6047a2f443074eebae51
soruto752 <sekoniireoluwa@gmail.com>
2026-08-16 21:21:52 +0100

    thee

---
+++ js/classwork.js
@@ -0,0 +1,32 @@
+//1
+const order = ["jollof rice,malt,chicken"];
+console.log(order[0]);
+console.log(order[order.length - 1]);
+order[1] = "coke";
+order.push("pepper soup");
+console.log(order);
+console.log(order.length);
+let orderstring = order.toString;
+console.log(orderstring);
+
+//2
+const stundent = ["david", "james", 15, "ss2"];
+let fullname = stundent[0] + " " + stundent[1];
+console.log(fullname);
+console.log(stundent[3]);
+console.log(stundent[4]);
+stundent[3] = "16";
+stundent[4] = "ss3";
+console.log(stundent);
+console.log(stundent.length);
+
+//3
+const product = ["Nike Air Max", 150000, "Shoes", true];
+console.log(product[0]);
+console.log(product[1]);
+console.log(product[2]);
+stundent[4] = false;
+console.log(product);
+let stundentstring = stundent.toString;
+console.log(stundentstring);
+console.log(student.length);
